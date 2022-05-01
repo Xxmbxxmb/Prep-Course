2cbf1f40b6aff37894f38641b13680908ca63009
Xxmbxxmb <diegocerdacelis@hotmail.com>
2022-05-01 09:10:21 -0400

    hw 10 finished

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -125,7 +125,14 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-}
+  arr.sort(function(a, b){
+    return a.length - b.length;
+
+  });
+  return arr
+  }
+
+
 
 
 function buscoInterseccion(arreglo1, arreglo2){
